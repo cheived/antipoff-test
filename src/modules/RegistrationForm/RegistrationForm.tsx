6b22006fc88e652d55c03ac0679d8a5aa0e8cf82
cheived <cheived55@gmail.com>
2024-07-11 21:37:28 +0400

    new: registration form

---
+++ src/modules/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react"
+import "./RegistrationForm.scss"
+import { Card } from "../../ui/Card"
+import clsx from "clsx"
+import { Button } from "../../ui/Button"
+import { Input } from "../../ui/Input"
+
+interface IRegistrationForm {
+    cn?: string
+}
+
+const RegistrationForm: FC<IRegistrationForm> = ({ cn }) => {
+    return (
+        <Card cn={clsx("form", cn)} title="Регистрация">
+            <form className={clsx("form__main-content")}>
+                <div className="form__main-content__item">
+                    <p className="form__main-content__item__title">Имя</p>
+                    <Input placeholder="Артур" />
+                </div>
+                <div className="form__main-content__item">
+                    <p className="form__main-content__item__title">Электронная почта</p>
+                    <Input placeholder="example@mail.ru" />
+                </div>
+                <div className="form__main-content__item" >
+                    <p className="form__main-content__item__title">Пароль</p>
+                    <Input type="password" />
+                </div >
+                <div className="form__main-content__item">
+                    <p className="form__main-content__item__title">Подтвердите пароль</p>
+                    <Input type="password" />
+                </div>
+            </form>
+            <Button cn={clsx("form__button")} >Зарегистрироваться</Button>
+        </Card>
+    )
+}
+
+export default RegistrationForm
