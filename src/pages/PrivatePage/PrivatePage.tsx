f04930c590a2775c8fc1b33577a950b02ccf320e
cheived <cheived55@gmail.com>
2024-07-15 23:48:24 +0400

    remove console.log

--- src/pages/PrivatePage/PrivatePage.tsx
+++ src/pages/PrivatePage/PrivatePage.tsx
@@ -1,14 +1,13 @@
 import { FC } from "react"
 import { Navigate } from "react-router-dom"
 import { useSelector } from "react-redux"
-
+import { RootState } from "../../store/store"
 interface IPrivatePage {
     element: React.ReactNode
 }
 
 const PrivatePage: FC<IPrivatePage> = ({ element: Element }) => {
-    const isAuthenticated = useSelector(state => state.user.isAuthenticated)
-    console.log(isAuthenticated)
+    const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated)
     return isAuthenticated ? Element : <Navigate to={'/registration'} />
 }
 
--- src/pages/UserInfo/UserInfo.tsx
+++ src/pages/UserInfo/UserInfo.tsx
@@ -6,34 +6,51 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import fetchUser from "./api/fetchUser"
 import IUser from "../../types/user"
+import { useSelector } from "react-redux"
+import { RootState } from "../../store/store"
+
 
 const UserInfo = () => {
     const { userId } = useParams();
-    console.log(userId)
+    const currentUser = useSelector((state: RootState) => state.user.user)
     const [user, setUser] = useState<IUser>();
+    const [avatarImg, setAvatarImg] = useState("")
 
     useEffect(() => {
         async function getUser() {
             const user = await fetchUser(userId || 1)
             setUser(user)
+            setAvatarImg(user?.avatar || "")
         }
         getUser()
     }, [userId])
 
-    // if (!user) {
-    //     return null
-    // }
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const imageUrl = reader.result as string;
+                setAvatarImg(imageUrl);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
     return (
         <div className={clsx("user-info", user && "user-info_visible")}>
-            <Header cn={clsx("user-info__header")} left="Назад" right="Выход" >
-                <div className="user-info__main-info">
-                    <Avatar cn="user-info__avatar" src={user?.avatar} width={178} height={178} />
-                    <div className="user-info__title-wrapper">
-                        <h1 className="user-info__name">{user?.first_name + " " + user?.last_name}</h1>
-                        <h2 className="user-info__position">Партнер</h2>
+            <Header cn={clsx("user-info__header")} left="Назад" >
+                <>
+                    <div className="user-info__main-info">
+                        <Avatar cn="user-info__avatar" src={avatarImg} width={178} height={178} />
+
+                        <div className="user-info__title-wrapper">
+                            <h1 className="user-info__name">{user?.first_name + " " + user?.last_name}</h1>
+                            <h2 className="user-info__position">Партнер</h2>
+                        </div>
                     </div>
-                </div>
+                    {userId == currentUser?.id ? <input className="user-info__avatar-upload" type="file" onChange={(event) => handleFileChange(event)} /> : null}
+                </>
             </Header >
             <div className="user-info__description-wrapper">
                 <h2 className="user-info__description">
@@ -49,11 +66,11 @@ const UserInfo = () => {
                 </h2>
                 <span className="user-info__contacts">
                     <span className="user-info__phone-number-wrapper">
-                        <img className="user-info__phone-icon" src="/img/phone.svg" />
+                        <img className="user-info__phone-icon" src="/img/ic_phone.svg" />
                         <h2 className="user-info__phone">+7 (954) 333-44-55</h2>
                     </span>
                     <span className="user-info__email-wrapper">
-                        <img className="user-info__email-icon" src="/img/mail.svg" />
+                        <img className="user-info__email-icon" src="/img/ic_mail.svg" />
                         <h2 className="user-info__email">{user?.email}</h2>
                     </span>
                 </span>
